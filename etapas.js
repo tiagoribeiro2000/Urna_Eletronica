94e28f0b118a429e4f40cabc2d7802a540ca3f44
Tiago Ribeiro <tgwebmaster27@gmail.com>
2021-08-16 17:44:31 -0300

    Projeto finalizado urna eletronica

---
+++ etapas.js
@@ -0,0 +1,54 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: 'cp3.jpg', legenda: 'Vereador' }
+                ]
+
+            },
+            {
+                numero: '77222',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: 'cp4.jpg', legenda: 'Vereador' }
+
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: 'cv1.jpg', legenda: 'Prefeito' },
+                    { url: 'cv2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+
+            },
+            {
+                numero: '84',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    { url: 'cv3.jpg', legenda: 'Prefeito' },
+                    { url: 'cv4.jpg', legenda: 'Vice-Prefeito', small: true }
+
+                ]
+            },
+        ]
+    }
+];
